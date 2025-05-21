c164a330b06f3567c13e60e3087e8af1e6d630be
NE7K <sjjang16@naver.com>
2025-05-22 02:01:04 +0900

    Feat : DeadProbability complete

--- DeadProbability.py
+++ DeadProbability.py
@@ -5,20 +5,20 @@ import numpy as np
 data = pd.read_csv('Data/train.csv')
 
 # .isnull() : null이면 True, .sum() 개수
-print(data.isnull().sum())
+# print(data.isnull().sum())
 
 # 빈칸에서 나이는 평균값
-print(data['Age'].mean())
+# print(data['Age'].mean())
 
 # 평균값 반올림해서 빈칸에 채우기
 data['Age'] = data['Age'].fillna(value=30)
 
 # 탑승구 최빈값
-print(data['Embarked'].mode())
+# print(data['Embarked'].mode())
 
 data['Embarked'] = data['Embarked'].fillna(value='S')
 
-print(data.isnull().sum())
+# print(data.isnull().sum())
 
 y = data.pop('Survived')
 
@@ -58,9 +58,9 @@ Ticket_Preprocessing = tf.keras.layers.StringLookup()
 Ticket_Preprocessing.adapt(np.array(data['Ticket']))
 
 # 유니크한 데이터 수
-print(len(data['Ticket'].unique()))
+# print(len(data['Ticket'].unique()))
 
-Ticket_Preprocessing = tf.keras.layers.Embedding(len(data['Ticket'].unique()) + 1, 10)
+Ticket_Embedding = tf.keras.layers.Embedding(len(data['Ticket'].unique()) + 1, 9)
 
 # functional api 사용할거라서 input 생성
 input_fare = tf.keras.Input(shape=(1,), name='Fare')
@@ -73,3 +73,45 @@ input_sex = tf.keras.Input(shape=(1,), name='Sex', dtype=tf.string)
 input_embarked = tf.keras.Input(shape=(1,), name='Embarked', dtype=tf.string)
 input_ticket = tf.keras.Input(shape=(1,), name='Ticket', dtype=tf.string)
 
+x_fare = Fare_Preprocessing(input_fare)
+x_parch = Parch_Preprocessing(input_parch)
+x_sibsp = Sib_Preprocessing(input_sibsp)
+x_pclass = Pclass_Preprocessing(input_pclass)
+x_age = Age_Preprocessing(input_age)
+# BUG age float32 맞춰주래
+x_age = tf.cast(x_age, tf.float32)
+x_sex = Sex_Preprocessing(input_sex)
+x_embarked = Embarked_Preprocessing(input_embarked)
+# ticket layer는 embedding을 또 거치기 때문에 그것도 이어줌
+x_ticket1 = Ticket_Preprocessing(input_ticket)
+x_ticket2 = Ticket_Embedding(x_ticket1)
+x_ticket3 = tf.keras.layers.Flatten()(x_ticket2)
+
+concat_layer = tf.keras.layers.concatenate([
+    x_fare, x_parch, x_sibsp, x_pclass, x_age, x_sex, x_embarked, x_ticket3
+])
+
+x = tf.keras.layers.Dense(128, activation='relu')(concat_layer)
+x = tf.keras.layers.Dense(64, activation='relu')(x)
+last_layer= tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+# 모델 시작과 마지막 표기
+model =tf.keras.Model(
+    inputs=[input_fare, input_parch, input_sibsp, input_pclass, input_age, input_sex, input_embarked, input_ticket],
+    outputs=last_layer
+)
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+xdata = {
+    'Fare' : np.array(data['Fare']),
+    'Age' : np.array(data['Age']),
+    'Parch' : np.array(data['Parch']),
+    'SibSp' : np.array(data['SibSp']),
+    'Pclass' : np.array(data['Pclass']),
+    'Sex' : np.array(data['Sex']),
+    'Embarked' : np.array(data['Embarked']),
+    'Ticket' : np.array(data['Ticket']),
+}
+
+model.fit(xdata, y, epochs=15, validation_split=0.1)
