2bc2f1351f8fd436fd5560dd2a3bc7a7b76f5eee
NE7K <sjjang16@naver.com>
2025-05-22 01:20:30 +0900

    Feat : Added input layer

--- DeadProbability.py
+++ DeadProbability.py
@@ -37,3 +37,39 @@ Pclass_Preprocessing.adapt(np.array(data['Pclass']))
 
 # print(Pclass_Preprocessing(np.array(data['Pclass'])))
 
+# 비슷한 숫자 묶음
+Age_Preprocessing = tf.keras.layers.Discretization(bin_boundaries=[10, 20, 30, 40, 50, 60])
+
+# Age 변환 체크
+# print(Age_Preprocessing(np.array(data['Age'])))
+
+# categoryencoding은 원핫인코딩해줌 카테고리별로
+
+# 문자 원핫인코딩
+Sex_Preprocessing = tf.keras.layers.StringLookup(output_mode='one_hot')
+Sex_Preprocessing.adapt(np.array(data['Sex']))
+
+Embarked_Preprocessing = tf.keras.layers.StringLookup(output_mode='one_hot')
+Embarked_Preprocessing.adapt(np.array(data['Embarked']))
+
+# Create Embedding layers : 종류가 많아
+# 데이터를 정수로 치환해줌
+Ticket_Preprocessing = tf.keras.layers.StringLookup()
+Ticket_Preprocessing.adapt(np.array(data['Ticket']))
+
+# 유니크한 데이터 수
+print(len(data['Ticket'].unique()))
+
+Ticket_Preprocessing = tf.keras.layers.Embedding(len(data['Ticket'].unique()) + 1, 10)
+
+# functional api 사용할거라서 input 생성
+input_fare = tf.keras.Input(shape=(1,), name='Fare')
+input_parch = tf.keras.Input(shape=(1,), name='Parch')
+input_sibsp = tf.keras.Input(shape=(1,), name='SibSp')
+input_pclass = tf.keras.Input(shape=(1,), name='Pclass')
+input_age = tf.keras.Input(shape=(1,), name='Age')
+# 문자는 , dtype=tf.string 붙임
+input_sex = tf.keras.Input(shape=(1,), name='Sex', dtype=tf.string)
+input_embarked = tf.keras.Input(shape=(1,), name='Embarked', dtype=tf.string)
+input_ticket = tf.keras.Input(shape=(1,), name='Ticket', dtype=tf.string)
+
